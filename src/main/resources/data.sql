b96f09c47c0eb67cb8709f5b3a39caf67e13237c
Thiago Alvoravel <t.alvoravel@gmail.com>
2020-09-27 19:09:15 -0300

    Implementação da regra de autenticação

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$9CotAN24iDjYPFzUTny/3uvisNoiUY/L1jg5lkxtd7AcZQkaGRWya');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
