939d219e2649ef63ce8378540fa758eb54e02ac0
Thiago Alvoravel <t.alvoravel@gmail.com>
2020-09-26 22:22:00 -0300

    Refatorando paginação na listagem dos tópicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,19 +1,18 @@
 package br.com.alura.forum.controller;
 
-import java.net.URI;
-import java.util.List;
-
-import java.util.Optional;
-
 import static java.util.Objects.isNull;
+
+import java.net.URI;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,9 +45,7 @@ public class TopicosController {
 	
 	@GetMapping
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
-			@RequestParam int pagina, @RequestParam int qtd) {
-		
-		Pageable paginacao = PageRequest.of(pagina, qtd);
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.DESC) Pageable paginacao) {
 		
 		if(isNull(nomeCurso)) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
